cc0810f1038479342e5fcdc220f84573b353a3bc
West Kraemer <WestKraemer@gmail.com>
2016-01-28 20:49:59 -0500

    deleting articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,14 +26,21 @@ class ArticlesController < ApplicationController
    end
 
    def update
-  @article = Article.find(params[:id])
+     @article = Article.find(params[:id])
 
-  if @article.update(article_params)
+    if @article.update(article_params)
     redirect_to @article
-  else
+    else
     render 'edit'
+    end
   end
-end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+  redirect_to articles_path
+  end
 
 private
   def article_params
